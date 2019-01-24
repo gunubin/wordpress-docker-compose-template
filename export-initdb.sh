ded2bfba83a45eaacbc91372b0661a4dd9ae0a6e
gunubin <kato@condesire.com>
2019-01-24 19:35:15 +0900

    ⏰

--- export-initdb.sh
+++ export-initdb.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-#docker-compose exec db /bin/bash -c 'mysqldump -pwordpress --databases wordpress 2> /dev/null' > ./docker-entrypoint-initdb.d/wordpress.sql 
 docker-compose exec db /bin/bash -c 'mysqldump -pwordpress --databases wordpress 2> /dev/null 1> /docker-entrypoint-initdb.d/wordpress.sql'
